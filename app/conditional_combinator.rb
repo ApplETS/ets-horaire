ff5959fa646133e67dcd808d9cd4c6d5efb1aa84
Krystian Czesak <krystianczesak@gmail.com>
2012-11-25 23:45:40 -0500

    Refactoring for nicer code

--- app/conditional_combinator.rb
+++ app/conditional_combinator.rb
@@ -1,7 +1,8 @@
 class ConditionalCombinator
 
   def self.find_combinations(values, set_size, &block)
-    loop_through(values, 0, values.size - set_size, [], &block).delete_if { |combination| combination.size != set_size }
+    combinations = loop_through(values, 0, values.size - set_size, [], &block)
+    combinations.keep_if { |combination| combination.size == set_size }
   end
 
   private
@@ -18,17 +19,22 @@ class ConditionalCombinator
     to_index == values.size - 1
   end
 
-  def self.add_to_possibilities(values, combination_stack)
-    combinations = values.collect { |value| combination_stack.dup << value if yield(combination_stack, value) }
-    combinations.delete_if { |combination| combination.nil? }
+  def self.add_to_possibilities(values, combination_stack, &block)
+    values.collect { |value| add_if_yield(combination_stack, value, &block) }
   end
 
   def self.continue_through_possibilities(values, from_index, to_index, combination_stack, &block)
-    values[from_index..to_index].each_with_index.collect do |value, index|
-      combination_stack_duplicate = combination_stack.dup
-      combination_stack_duplicate << value if yield(combination_stack, value)
+    combination_stack = values[from_index..to_index].each_with_index.collect do |value, index|
+      combination_stack_duplicate = add_if_yield(combination_stack, value, &block)
       loop_through(values, from_index + index + 1, to_index + 1, combination_stack_duplicate, &block)
-    end.flatten(1)
+    end
+    combination_stack.flatten(1)
+  end
+
+  def self.add_if_yield(combination_stack, value, &block)
+    combination_stack_duplicate = combination_stack.dup
+    combination_stack_duplicate << value if yield(combination_stack_duplicate, value)
+    combination_stack_duplicate
   end
 
 end

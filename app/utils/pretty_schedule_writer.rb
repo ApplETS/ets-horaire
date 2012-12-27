cd1d5b4fa79e4eabd319555fecf937276d22b4e0
Krystian Czesak <krystianczesak@gmail.com>
2012-12-27 00:19:39 -0500

    Modified the pretty printer.

--- app/utils/pretty_schedule_writer.rb
+++ app/utils/pretty_schedule_writer.rb
@@ -5,18 +5,19 @@ class PrettyScheduleWriter
   def self.output(schedules, output_filename)
     File.open(output_filename, 'w') do |file|
       schedules.each do |groups|
-        file.write 43.times.collect { "*" }.join + "\n"
-        file.write 43.times.collect { "*" }.join + "\n"
+        file.write 58.times.collect { "*" }.join + "\n"
+        file.write 58.times.collect { "*" }.join + "\n"
         file.write "\n"
 
         groups.each do |group|
           file.write "#{group.course_name}\n"
-          file.write 43.times.collect { "-" }.join + "\n"
+          file.write 58.times.collect { "-" }.join + "\n"
           group_zerofilled = group.nb.to_s.rjust(2, "0")
           file.write "#{spacify(group_zerofilled)}#{output_period(group.periods[0])}"
           group.periods[1..-1].each do |period|
             file.write "#{spacify}#{output_period(period)}"
           end
+          file.write "\n"
           file.write "\n"
         end
       end
@@ -30,7 +31,7 @@ class PrettyScheduleWriter
   end
 
   def self.spacify(text = "")
-    spaces = (10 - text.length).times.collect { " " }.join
+    spaces = (15 - text.length).times.collect { " " }.join
     "#{text}#{spaces}"
   end
 

69b822abf3fcdb496208bb4e6a1272a85a24262f
Krystian <krystianczesak@gmail.com>
2013-02-28 20:59:12 -0500

    The pdftotext command now pipes through stdout: no need to output to a file and read the file.

--- app/utils/convert.rb
+++ app/utils/convert.rb
@@ -1,20 +1,7 @@
 class Convert
 
   def self.pdf_to_text(pdf_path)
-    input_path = "#{pdf_path}.pdf"
-    output_path = "#{pdf_path}.txt"
-    command_output = nil
-
-    execute_command(input_path, output_path) { |stdin| command_output = stdin.gets }
-
-    raise "Warning! pdftotext executed with output #{command_output}" unless command_output.nil?
-    File.open(output_path, "r")
-  end
-
-  private
-
-  def self.execute_command(input_path, output_path, &block)
-    IO.popen("pdftotext -enc UTF-8 -layout #{input_path} #{output_path}", &block)
+    IO.popen("pdftotext -enc UTF-8 -layout #{pdf_path}.pdf \"-\"")
   end
 
 end
